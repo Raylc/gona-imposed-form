05d4ff292c687d22c22bc6a8fbb39cecee07e25f
Cheng Liu <raylc1996@outlook.com>
2023-09-29 15:56:08 -0400

    merged the caliper and 3d datasets.

--- code/Analysis.R
+++ code/Analysis.R
@@ -21,3 +21,44 @@ variable_loadings<-data.frame(res.var$coord)
 pca2 <- psych::principal(gona_esa[1:7], nfactors=2, rotate="varimax", scores=F)
 pca2
 
+
+# Data preparation
+## Loading data
+gona_esa<-readr::read_csv("data/EA_Reduction_Paper_Order.csv")
+gona_3dgmm<-readr::read_csv("data/Gona3Ddata_GMM.csv")
+gona_scars<-readr::read_csv("data/Gona3Ddata_scars.csv")
+## creating a new column of unique identifier for merging
+gona_esa$Name <- paste(gona_esa$Site, gona_esa$Catalog, sep= "-")
+## Subsetting scar data frame because starting from th 7th column it lists the area of individual scars on an artifact, the number of which vary between artifacts.
+gona_scars_6c<-gona_scars[1:6,]
+## Manually editing the names of three observations because of errors in naming
+gona_esa[146,44]<-'BSN17-35' # It was BSN17-17C-35 before
+gona_esa[136,44]<-'BSN17C-29' # It was BSN17-17C-29 before
+gona_esa[133,44]<-'BSN17C-231' # It was BSN17-17c-231 before
+
+
+## Merging caliper and 3d measurements
+joined_gona <-dplyr::left_join(gona_esa, gona_3dgmm, by = c("Name" = "Name"))
+full_gona <-dplyr::left_join(joined_gona, gona_scars_6c, by = c("Name" = "Name"))
+## Subsetting a dataframe that contains both capliper and 3d measurements.
+overlap_gona <- full_gona %>% tidyr::drop_na(Type.y)
+write.csv(overlap_gona, file = "data/overlap_gona.csv")
+
+# Evaluating the correlations in PC values between caliper and 3d methods.
+ggstatsplot::ggscatterstats(
+  data  = overlap_gona,
+  x     = "FAC1_1.x",
+  y     = "3DGMPC1(10x10)",
+  xlab  = "Caliper PC1",
+  ylab  = "3DGMM PC1",
+)
+ggplot2::ggsave("3D PC1 correlation.png", path="figure.", width = 5, height = 5, dpi = 600)
+
+ggstatsplot::ggscatterstats(
+  data  = overlap_gona,
+  x     = "FAC2_1",
+  y     = "3DGMPC2(10x10)",
+  xlab  = "Caliper PC2",
+  ylab  = "3DGMM PC2",
+)
+ggplot2::ggsave("3D PC2 correlation.png", path="figure.", width = 5, height = 5, dpi = 600)
